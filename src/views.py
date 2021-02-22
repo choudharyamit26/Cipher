f3c00226ab637cbe5aab8344b59b6bd53e1c6654
choudharyamit26 <ravichoudhary766@gmail.com>
2021-02-22 12:39:15 +0530

    Modified compose message and inbox api

--- src/views.py
+++ src/views.py
@@ -663,7 +663,7 @@ class ReadingMessage(CreateAPIView):
                                 #     mode=message_obj.mode
                                 # )
                                 return Response({"message": "Correct answer", 'message_text': message_obj.text,
-                                                 'message_attachment': message_obj.attachment,
+                                                 'message_attachment': message_obj.attachment.url,
                                                  'sender_name': message_obj.sender.username, 'status': HTTP_200_OK})
                             else:
                                 return Response(
