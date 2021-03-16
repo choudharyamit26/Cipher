817ff245b5d1c9bb780895bff43fee9e40cc52c9
choudharyamit26 <ravichoudhary766@gmail.com>
2021-03-16 12:20:31 +0530

    Modified remove message

--- src/views.py
+++ src/views.py
@@ -787,9 +787,9 @@ class ReadingMessage(CreateAPIView):
                             # data_message = json.dumps(data_message)
                             title = "Message Read"
                             # body = f'{app_user_obj.username} read your message'
-                            body = f'{app_user_obj.username} read your message' + 'Message Sent:' + str(
-                                message_obj.created_at) + ', ' + str(message_obj.mode) + ':' + str(
-                                [x.username for x in message_obj.receiver.all()])
+                            body = f'{app_user_obj.username} read your message' + '  Message Sent:' + str(
+                                message_obj.created_at.strftime("%B %d, %y.")) + ', ' + str(message_obj.mode) + ':' + str(", ".join(
+                                [x.username for x in message_obj.receiver.all()]))
                             message_type = "messageRead"
                             respo = send_another(
                                 fcm_token, title, body, message_type)
