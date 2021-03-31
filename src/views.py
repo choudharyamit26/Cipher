24f51e97ff48eaf6168b5445fbb52ee7aead4dc3
choudharyamit26 <ravichoudhary766@gmail.com>
2021-03-31 18:17:26 +0530

    Added quizlok image

--- src/views.py
+++ src/views.py
@@ -754,12 +754,12 @@ class ReadingMessage(CreateAPIView):
                                                             # body = f'{app_user_obj.username} read your message' + 'Message Sent:' + str(
                                                             #     message_obj.created_at) + ', ' + str(message_obj.mode) + ':' + str(
                                                             #     [x.username for x in message_obj.receiver.all()])
-                                                            body = f'{app_user_obj.username} got your secret question wrong 3 times so the message has been terminated forever' + ' Message Sent: ' + str(
+                                                            body = f'{app_user_obj.username} got your secret question wrong 3 times so the message has been terminated forever.' + ' Message Sent: ' + str(
                                                                 message_obj.created_at.strftime(
                                                                     "%B %d, %Y.")) + ' @ ' + str(
                                                                 message_obj.created_at.strftime(
                                                                     "%I:%-M%p")) + ' ' + str(
-                                                                message_obj.mode) + ':' + str(", ".join(
+                                                                message_obj.mode) + ': ' + str(", ".join(
                                                                 [x.username for x in message_obj.receiver.all()]))
                                                             message_type = "messageRead"
                                                             sound = 'notifications.mp3'
