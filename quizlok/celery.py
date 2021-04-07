2a343258f1eee630a985bbcc1c308ac53771aec6
choudharyamit26 <ravichoudhary766@gmail.com>
2021-04-07 17:15:28 +0530

    pushing code to development server

--- quizlok/celery.py
+++ quizlok/celery.py
@@ -13,8 +13,8 @@ app = Celery('quizlok')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.config_from_object('django.conf:settings')
 app.conf.beat_schedule = {
     'delete-message': {
         'task': 'src.tasks.expire_messages',
