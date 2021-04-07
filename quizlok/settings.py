4eb437e014a9f25a042eded7bb991f2a5fbbd32d
choudharyamit26 <ravichoudhary766@gmail.com>
2021-04-07 11:08:35 +0530

    pushing code to development server

--- quizlok/settings.py
+++ quizlok/settings.py
@@ -81,24 +81,24 @@ WSGI_APPLICATION = 'quizlok.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Quizlok Production Server
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'quizlok',
-#         'USER': 'quizlok_user',
-#         'PASSWORD': 'quizlok',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# Quizlok Production Server
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quizlok',
+        'USER': 'quizlok_user',
+        'PASSWORD': 'quizlok',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
