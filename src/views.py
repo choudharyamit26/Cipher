e15c0d87804f2d193a6f10a36cc905a219ee9f86
choudharyamit26 <ravichoudhary766@gmail.com>
2021-04-28 23:39:46 +0530

    added time in inappnotifications

--- src/views.py
+++ src/views.py
@@ -460,7 +460,7 @@ class ComposeMessage(CreateAPIView):
                     # ct = t.localize(timezone.now())
                     print(sender.user_timezone, ct)
                     # print(timezone.localtime(sender.user_timezone.now()))
-                    print(timezone.localtime(pytz.timezone(sender.user_timezone)))
+                    # print(timezone.localtime(pytz.timezone(sender.user_timezone)))
                     print(timezone.localtime(pytz.timezone(sender.user_timezone).now()))
                     msg_obj = Message.objects.create(
                         sender=sender,
